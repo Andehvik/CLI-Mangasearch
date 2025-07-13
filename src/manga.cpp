11714b3672e606b8413343749b89d070e4dc014d
Anders Høvik <andershvik@gmail.com>
2025-07-13 22:55:01 +0200

    fixed errors in constructor

--- src/manga.cpp
+++ src/manga.cpp
@@ -1,15 +1,15 @@
 #include "manga.h"
 #include <string>
 
-manga::manga(std::string name, std::string description, int rating)
+manga::manga(std::string name, std::string description, int rating) :
 name(name),
 description(description),
 rating(rating){}
 
 manga manga::get_manga() const{
-	return manga;
+	return *this;
 }
 
 void manga::rename_name(std::string new_name){
-	this.name = new_name;
+	this->name = new_name;
 }
