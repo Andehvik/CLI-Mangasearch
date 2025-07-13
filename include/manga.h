f9f91dd1e1e3f2e182e98f4d4aaa73113092ceda
Anders Høvik <andershvik@gmail.com>
2025-07-13 22:48:39 +0200

    written header and impl to manga

--- include/manga.h
+++ include/manga.h
@@ -1,12 +1,21 @@
+#ifndef manga_H
+#define manga_H
+
 #include <string>
 
 class manga{
 public:
-	<manga> get_manga() const;
 
-	void delete_manga(manga selection) const;
+	manga(std::string name, std::string description, int rating);
+
+	manga get_manga() const;
+
+	void rename_name(std::string new_name);
 private:
+
 std::string name;
 std::string description;
 int rating;
-}
+};
+
+#endif
---
+++ src/manga.cpp
@@ -0,0 +1,15 @@
+#include "manga.h"
+#include <string>
+
+manga::manga(std::string name, std::string description, int rating)
+name(name),
+description(description),
+rating(rating){}
+
+manga manga::get_manga() const{
+	return manga;
+}
+
+void manga::rename_name(std::string new_name){
+	this.name = new_name;
+}
